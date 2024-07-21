781e2a5304bb62702822c34c2da9bc3417aa8ebc
Andre Polykanine <andre@accessmind.net>
2024-07-22 01:33:17 +0200

    Add pseudo menu for entering operators

--- app/src/main/java/org/oire/calculette/MainActivity.java
+++ app/src/main/java/org/oire/calculette/MainActivity.java
@@ -1,14 +1,16 @@
 package org.oire.calculette;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.text.Editable;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ListView;
 import java.util.Locale;
 import org.mariuszgromada.math.mxparser.Expression;
 
@@ -50,26 +52,79 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         result = expression.calculate();
                         String resultString = String.valueOf(result);
-                        String message = getString(R.string.result_announcement, result);
+                        String message = getString(R.string.result_announcement, resultString);
                         Log.d(TAG, String.format("Result message: %s", message));
-                        tts.speak(message, TextToSpeech.QUEUE_FLUSH, null);
                         expressionEditBox.setText(resultString);
                         expressionEditBox.setSelection(expressionEditBox.getText().length());
+                        tts.speak(message, TextToSpeech.QUEUE_FLUSH, null, null);
                         return true;
                     } catch (Exception e) {
                         Log.e(TAG, "Unable to calculate", e);
-                        tts.speak(getString(R.string.error), TextToSpeech.QUEUE_FLUSH, null);
+                        tts.speak(getString(R.string.error), TextToSpeech.QUEUE_FLUSH, null, null);
                     }
+                } else if (event.isCtrlPressed() && keyCode == KeyEvent.KEYCODE_M) {
+                    Dialog pseudoMenu = new Dialog(MainActivity.this);
+                    pseudoMenu.setContentView(R.layout.pseudo_popup_menu);
+
+                    ListView menuList = pseudoMenu.findViewById(R.id.menuList);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, new String[]{"+", "-", "*", "/"});
+                    menuList.setAdapter(adapter);
+
+                    menuList.setOnKeyListener((mv, mKeyCode, mEvent) -> {
+                        if (mEvent.getAction() == KeyEvent.ACTION_UP) {
+                            int position = menuList.getSelectedItemPosition();
+                            int newPosition = 0;
+                            switch (mKeyCode) {
+                                case KeyEvent.KEYCODE_DPAD_DOWN:
+                                    if (position < adapter.getCount() - 1) {
+                                        newPosition = position + 1;
+                                    } else {
+                                        newPosition = 0;
+                                    }
+                                    menuList.setSelection(newPosition);
+                                    tts.speak(adapter.getItem(newPosition), TextToSpeech.QUEUE_FLUSH, null, null);
+                                    break;
+                                case KeyEvent.KEYCODE_DPAD_UP:
+                                    if (position > 0) {
+newPosition = position - 1;
+                                    } else {
+                                        newPosition = adapter.getCount() - 1;
+                                    }
+                                    menuList.setSelection(newPosition);
+                                    tts.speak(adapter.getItem(newPosition), TextToSpeech.QUEUE_FLUSH, null, null);
+                                    break;
+                                case KeyEvent.KEYCODE_ENTER:
+                                String operator = adapter.getItem(position);
+                                int cursorPosition = expressionEditBox.getSelectionStart();
+                                expressionEditBox.getText().insert(cursorPosition, operator);
+                                expressionEditBox.setSelection(cursorPosition + 1);
+                                tts.speak(operator, TextToSpeech.QUEUE_FLUSH, null, null);
+                                pseudoMenu.dismiss();
+                                return true;
+                            }
+                        }
+                        return true;
+                    });
+
+                    pseudoMenu.show();
+                    tts.speak(getString(R.string.context_menu), TextToSpeech.QUEUE_FLUSH, null, null);
+                } else if (keyCode == KeyEvent.KEYCODE_DEL || keyCode == KeyEvent.KEYCODE_FORWARD_DEL) {
+                    if (expressionEditBox.getText().length() > 0) {
+                        int start = Math.max(expressionEditBox.getSelectionStart(), 0);
+                        if (start > 0) {
+                            String deletedChar = expressionEditBox.getText().toString().substring(start - 1, start);
+                            tts.speak(getString(R.string.char_deleted, deletedChar), TextToSpeech.QUEUE_FLUSH, null, null);
+                        }
+                    }
+                    return false;
                 } else {
                     // Get the pressed key character and speak it
                     char keyChar = (char) event.getUnicodeChar();
-                    tts.speak(String.valueOf(keyChar), TextToSpeech.QUEUE_FLUSH, null);
+                    tts.speak(String.valueOf(keyChar), TextToSpeech.QUEUE_FLUSH, null, null);
                 }
             }
             return false;
         });
-
-
     }
 
     @Override
